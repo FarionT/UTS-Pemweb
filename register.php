bd270eea28cac75fe37c3ebab772624d5f85506e
FarionT <farion2803@gmail.com>
2022-10-08 13:25:04 +0700

    register

---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="shadow w-100 d-flex justify-content-between py-2">
+        <a href="#" class="ms-5"><img style="width: 190px; height: 50px;" src="img/logo.png"/></a>
+        <div class="w-50 d-flex justify-content-between">
+            <a href="#" class="h3 text-body text-decoration-none mt-2">ALL</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">C</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">PHP</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">Python</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">Java</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">Javascript</a>
+        </div>
+        <div class="d-flex me-5">
+            <a href="#" class="h2 text-body text-decoration-none mt-2">Create</a>
+            <h2 class="mt-2">&nbsp;|&nbsp;</h2>
+            <a href="#" class="h2 text-body text-decoration-none mt-2">Log In</a>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
