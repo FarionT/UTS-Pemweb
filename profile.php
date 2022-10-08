031c48793d2a1cdafe58a5d3055f52394d84881f
FarionT <farion2803@gmail.com>
2022-10-08 15:59:42 +0700

    profile

---
+++ profile.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body style="background-color:#D9D9D9">
+    <nav class="shadow w-100 d-flex justify-content-between py-2" style="background-color:#FFFFFF">
+        <a href="#" class="ms-5"><img style="width: 190px; height: 50px;" src="img/logo.png"/></a>
+        <div class="w-50 d-flex justify-content-between">
+            <a href="#" class="h3 text-body text-decoration-none mt-2">ALL</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">C</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">PHP</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">Python</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">Java</a>
+            <a href="#" class="h3 text-body text-decoration-none mt-2">Javascript</a>
+        </div>
+        <div class="d-flex me-5">
+            <a href="#" class="h2 text-body text-decoration-none mt-2">Create</a>
+            <h2 class="mt-2">&nbsp;|&nbsp;</h2>
+            <a href="#" class="h2 text-body text-decoration-none mt-2">Log In</a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="my-5 p-5" style=" height:200px;background-image:url('img/bg_profile.png');border-radius:20px;">
+
+            <img src="img/default.png" class="d-inline-block my-auto" style="width:10%" />
+
+            <div class="d-inline-block">
+                <h4>Atong</h4>
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+        </div>
+        <div class="d-flex justify-content-around">
+            <div id="kiri" style="width:70%" class="d-inline-block">
+                <div class="bg-white p-3 mb-2" style="width:100%;height:auto">
+                    <div class="d-flex">
+                        <img src="img/default.png" class="d-inline-block align-middle" style="width:6%;height:6%"/>
+                        <div>
+                            <p class="mb-0 align-middle">Atong | PHP</p>
+                            <p>05 - 10 - 2022 13:23</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>PHP adalah bahasa pemrograman yang diciptakan pada tahun 1995</p>
+                    </div>
+                    <div>
+                        <p class="d-inline">❤️10</p>
+                        <p class="d-inline">✉️3</p>
+                    </div>
+                </div>
+                <div class="bg-white p-3 mb-2" style="width:100%;height:auto">
+                    <div class="d-flex">
+                        <img src="img/default.png" class="d-inline-block align-middle" style="width:6%;height:6%"/>
+                        <div>
+                            <p class="mb-0 align-middle">Atong | PHP</p>
+                            <p>05 - 10 - 2022 13:23</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>PHP adalah bahasa pemrograman yang diciptakan pada tahun 1995</p>
+                    </div>
+                    <div>
+                        <p class="d-inline">❤️10</p>
+                        <p class="d-inline">✉️3</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="kanan"  style="width:25%;" class="justify-content-center">
+                <div class="bg-white text-center mb-3 d-inline-block" style="width:100%">
+                    <p>Postingan</p>
+                    <p>1</p>
+                </div>
+                <br/>
+                <div class="bg-white text-center d-inline-block" style="width:100%">
+                    <p>Username: atong123</p>
+                    <p>Email: atong@student.umn.ac.id</p>
+                    <a href="#" style="text-decoration:none">✏️Edit Profile</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="d-flex justify-content-end" style="background-color: #000000; position: fixed;  bottom: 0; width: 100%;">
+        <p class="mt-2 mb-3 me-5">Site design/logo by ©Ngoding Coy 2022 Inc</p>
+    </footer>
+</body>
+</html>
